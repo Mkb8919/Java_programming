a436edf37ab2451f61de5441cf5aaf05cdb17c30
Mohit <mohitbhatia659@gmail.com>
2024-11-04 23:44:15 +0530

    Day 4

---
+++ Day 4/cwh13_ch3_strings.java
@@ -0,0 +1,27 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class cwh13_ch3_strings {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+      //  String name = new String("Mohit"); or
+       // String name = "Mohit";
+       // System.out.print("The name is : ");
+      //  System.out.print(name); // we used println for making space in outputs
+       int a = 6;
+      float b = 5.6454f;
+       System.out.printf("The value of a is %d and value of b is %8.2f" , a,b);
+       // System.out.format("The value of a is %d and value of b is %f", a,b);
+
+        // enter string from user
+        //String st = sc.next();
+        //String st = sc.nextLine();
+       // System.out.println(st);
+
+
+
+
+
+    }
+}
---
+++ Day 4/cwh14_ch3_strings_methods.java
@@ -0,0 +1,50 @@
+package com.company;
+
+public class cwh14_ch3_strings_methods {
+    public static void main(String[] args) {
+        String name = "Harry";
+       // System.out.println(name);
+       // int value = name.length();
+       //System.out.println(value);
+
+      // String lstring = name.toLowerCase();
+      //  System.out.println(lstring);
+
+        //String ustring = name.toUpperCase();
+       //System.out.println(ustring);
+
+       //String nonTrimmedString = "    Mohit   ";
+       //System.out.println(nonTrimmedString);
+
+       // String trimmedString = nonTrimmedString.trim();
+      // System.out.println(trimmedString);
+
+       //System.out.println(name.substring(1));
+      // System.out.println(name.substring(1,4));
+
+       // System.out.println(name.replace('r','p'));
+     //  System.out.println(name.replace("r","ier"));
+
+       // System.out.println(name.startsWith("Har"));
+      // System.out.println(name.endsWith("y"));
+
+      //  System.out.println(name.charAt(2));
+
+      // String modifiedName = "Harryrryrry";
+      //  System.out.println(modifiedName.indexOf("r"));
+      //  System.out.println(modifiedName.indexOf("rry",4));
+      //  System.out.println(modifiedName.lastIndexOf("r"));
+      //  System.out.println(modifiedName.lastIndexOf("rry",7));
+
+       // System.out.println(name.equals("Harry"));
+       // System.out.println(name.equalsIgnoreCase("harry"));
+
+       //System.out.println("Iam escape sequence \\double quote ");
+     //  System.out.println("Iam escape sequence \"double quote ");
+     //  System.out.println("Iam escape sequence \ndouble quote ");
+      // System.out.println("Iam escape sequence \tdouble quote ");
+
+
+
+    }
+}

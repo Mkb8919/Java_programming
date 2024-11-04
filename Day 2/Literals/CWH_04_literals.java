e700b14e3a0ad5dcf8da840b32532d4b21449422
Mohit <mohitbhatia659@gmail.com>
2024-11-04 23:42:14 +0530

    Day 2

---
+++ Day 2/Literals/CWH_04_literals.java
@@ -0,0 +1,19 @@
+package com.company;
+
+public class CWH_04_literals {
+    public static void main(String[] args) {
+        byte age = 34;
+        int age2 = 56;
+        short age3 = 87;
+        long ageDino = 56666666L;
+        char ch = 'A';
+        float f1 = 5.6f;
+        double d1 = 4.66d;
+
+        boolean a = true;
+        System.out.println(age);
+        System.out.println(age2);
+        String str = "Mohit";
+        System.out.println(str);
+    }
+}
---
+++ Day 2/Variables and Datatypes/cwh_3_Variables_and_datatypes.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class cwh_3_Variables_and_datatypes {
+    public static void main(String[] args) {
+        System.out.println("The sum of these numbers are:");
+        int num1 = 6;
+        int num2 = 7;
+        int num3 = 8;
+        int sum = num1 +num2 + num3 ;
+        System.out.println(sum);
+    }
+}
